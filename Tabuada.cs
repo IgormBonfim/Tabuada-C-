adf610843ea4298ff2573449f3ffe3d89811ee94
Igor Bonfim <83429569+IgormBonfim@users.noreply.github.com>
2022-07-15 13:03:15 -0300

    Add files via upload

---
+++ Tabuada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabuadaCs
+{
+    class Tabuada
+    {
+        static void Main(string[] args)
+        {
+
+            int tabuada;
+
+            Console.WriteLine("Iniciando Algoritmo de tabuada");
+            Console.WriteLine("Escreva o número da tabuada");
+            tabuada = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tabuada de " + tabuada + " é: ");
+            for(int i = 0; i <= 10; i++) {
+                Console.WriteLine(tabuada + " X " + i + " = " + tabuada * i);
+            }
+        }
+    }
+}
